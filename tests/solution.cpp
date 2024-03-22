1c82ca949f57ca272ca4135f1cab4cfe3fddef37
colobocococo <120367114+colobocococo@users.noreply.github.com>
2024-03-22 15:48:21 +0500

    Update solution.cpp

--- tests/solution.cpp
+++ tests/solution.cpp
@@ -1,10 +1,30 @@
+#include <gtest/gtest.h>
 #include "../src/stack1.cpp"
 using namespace std;
 
+Stack <int> q;
+Stack <int> w;
 
+TEST(HelloTest, BasicAssertions) {
+    EXPECT_EQ(q.isEmpty(), w.isEmpty());
+}
 
-int main() {
-    Stack <int> s;
-    s.push2(move(1));
-    return 0;
+TEST(a1, b1) {
+    q.push(1);
+    EXPECT_EQ(q.isEmpty(), !w.isEmpty());
+}
+
+TEST(a2, b2) {
+    w.push(1);
+    EXPECT_EQ(q.peek(), w.peek());
+}
+
+TEST(a3, b3) {
+    q.pop();
+    EXPECT_EQ(q.isEmpty(), true);
+}
+
+TEST(a4, b4) {
+    q.pop();
+    EXPECT_EQ(w.isEmpty(), false);
 }
