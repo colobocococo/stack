f0f7220e6c7841c214921ab3760f3268aeac2bcd
colobocococo <120367114+colobocococo@users.noreply.github.com>
2024-03-22 19:33:29 +0500

    Update solution.cpp

--- tests/solution.cpp
+++ tests/solution.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 Stack <int> q;
 Stack <int> w;
+Stack <char> s;
 
 TEST(HelloTest, BasicAssertions) {
     EXPECT_EQ(q.isEmpty(), w.isEmpty());
@@ -27,4 +28,20 @@ TEST(a3, b3) {
 TEST(a4, b4) {
     q.pop();
     EXPECT_EQ(w.isEmpty(), false);
+}
+
+TEST(a5, b5) {
+    for (int i = 0; i < 24; i++) {
+        s.push(char('a' + i));
+    }
+
+    EXPECT_EQ(s.peek(), 'x');
+}
+
+TEST(a6, b7) {
+    for (int i = 0; i < 22; i++) {
+        s.pop();
+    }
+
+    EXPECT_EQ(s.peek(), 'b');
 }
